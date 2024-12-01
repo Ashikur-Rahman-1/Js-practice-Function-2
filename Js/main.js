f65dc73795d21bdb6bd3ca1bc70418eb14f8dafd
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2024-12-01 20:30:15 +0600

    passing code

---
+++ Js/main.js
@@ -0,0 +1,25 @@
+// function expression
+
+function sumMaker(num1, num2) { //parameter
+  
+  let sum = num1 + num2;
+  return sum;
+}
+
+// calling function
+
+
+function divisionMaker(num1) { //parameter
+  return (div = num1/100);
+  
+}
+
+//recieving return value
+let returnsumvalue = sumMaker(20, 2); 
+let divOutput = divisionMaker(returnsumvalue);
+console.log(divOutput);
+// calling function
+
+  // argument passing
+
+  
